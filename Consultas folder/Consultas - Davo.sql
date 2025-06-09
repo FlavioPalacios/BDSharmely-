f865dbb15b11f9d6c2cb0fb2b0ae9c872724ee21
JoseJoseQuispeDavila <023101527i@uandina.edu.pe>
2025-06-08 22:52:07 -0500

    Update Consultas - Davo.sql

--- Consultas folder/Consultas - Davo.sql
+++ Consultas folder/Consultas - Davo.sql
@@ -1,5 +1,7 @@
-
------------------------
+-- Consulta 1: Selecciona productos y los ordena por precio.
+-- Extrae el código, nombre, precio y stock de todos los productos.
+-- La cláusula ORDER BY precioProducto DESC organiza los resultados
+-- desde el producto más caro al más barato.
 SELECT
     codeProducto,
     nombreProducto,
@@ -9,7 +11,13 @@ FROM
     Producto
 ORDER BY
     precioProducto DESC;
+
 -----------------------
+
+-- Consulta 2: Muestra una lista de clientes ordenada alfabéticamente.
+-- Selecciona la información principal de los clientes.
+-- La cláusula ORDER BY apellidoCliente ASC ordena la lista de clientes
+-- en orden alfabético ascendente (A-Z) según su apellido.
 SELECT
     idCliente,
     nombreCliente,
@@ -19,7 +27,14 @@ FROM
     Cliente
 ORDER BY
     apellidoCliente ASC;
+
 -----------------------
+
+-- Consulta 3: Muestra los pedidos más recientes primero.
+-- Obtiene los datos más importantes de cada pedido.
+-- La cláusula ORDER BY fechaPedido DESC organiza los pedidos
+-- de forma cronológica inversa, mostrando los que se hicieron
+-- más recientemente en la parte superior de la lista.
 SELECT
     idPedido,
     fechaPedido,
